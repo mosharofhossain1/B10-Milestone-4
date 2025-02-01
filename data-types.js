d41e1b33f45f6fdeba29eb0cc10d45ab0203c54a
Mosharof <md5470415@gmail.com>
2025-02-01 09:16:09 +0600

    js basic data types practice

---
+++ data-types.js
@@ -0,0 +1,16 @@
+/* জাভাস্ক্রিপ্টে ৬ ধরনের প্রিমিটিভ ডাটা টাইপস রয়েছে: */
+/* ***
+        1.String
+        2.Number
+        3.Boolean
+        4.null
+        5.undifined
+        6.object 
+** */
+
+let stirng = "Hello";
+let number = 34;
+let isCool = true;
+let n_ull = null;
+let undif = undefined;
+let object = { name: 'Mosharof', age: 23 }
--- variable.js
+++ variable.js
@@ -1,16 +1,32 @@
-// There are Declaring 3 types of variables
+// ভ্যারিয়েবল কী?
+/* 
+ভ্যারিয়েবল হল একটি কন্টেইনার বা স্টোরেজ, যেখানে আমরা ডাটা সংরক্ষণ করি।
+
+ভ্যারিয়েবল ডিক্লেয়ার করার তিনটি উপায়:
+var (পুরানো, বেশি ব্যবহার হয় না)
+let (আধুনিক এবং বেশি ব্যবহার করা হয়)
+const (যখন পরিবর্তন প্রয়োজন নেই)
+ */
+
+/* 
+    var name = "Rahim"; // পুরানো স্টাইল
+    let age = 25; // আধুনিক
+    const country = "Bangladesh"; // কখনো পরিবর্তন হবে না
+
+*/
+
 
 // Number-1: var 
 var age = 45;
-var herName = "Suhan Khan";
+var friendName = "Suhan Khan";
 var isCool = true;
 
 // Number-2: let 
-let age = 23;
+let myAge = 23;
 let hisName = "Rukshana Begume";
 let isHot = false;
 
 // Number-3: const 
 const herName = 'Baki Billah';
 const A_lquran = true;
-
+console.log(A_lquran);
