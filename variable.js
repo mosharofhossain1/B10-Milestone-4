80629d924cf4bc4137f6997941c76cab94923a31
Mosharof <md5470415@gmail.com>
2025-02-01 09:05:52 +0600

    varible declaration type practice

---
+++ variable.js
@@ -0,0 +1,16 @@
+// There are Declaring 3 types of variables
+
+// Number-1: var 
+var age = 45;
+var herName = "Suhan Khan";
+var isCool = true;
+
+// Number-2: let 
+let age = 23;
+let hisName = "Rukshana Begume";
+let isHot = false;
+
+// Number-3: const 
+const herName = 'Baki Billah';
+const A_lquran = true;
+
